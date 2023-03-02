72ae7783fb8436ca78f150f00eae8f60fb6d6204
firmankhoiril <firmankhoiril13@gmail.com>
2023-03-02 18:05:12 +0700

    change url

--- src/api/fetchServer.js
+++ src/api/fetchServer.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/";
+const url = "https://firmanbackendapp20230423.netlify.app/.netlify/functions/api";
 
 export const fetchPost = async () => await axios.get(url);
 
